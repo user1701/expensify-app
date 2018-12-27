233996fc5c4ed9fd78bd9174f1d770c81f594986
Ivan Karaman <ivanko1992@gmail.com>
2018-12-27 19:28:20 +0200

    added test cases for reducers, selectors

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 import { setStartDate, setEndDate, setTextFilter, sortByDate, sortByAmount } from '../../actions/filters';
 
-test('should generate start date action object', () => {
+test('should generate set start date action object', () => {
     const action = setStartDate(moment(0));
     expect(action).toEqual({
         type: "SET_START_DATE",
@@ -9,7 +9,7 @@ test('should generate start date action object', () => {
     });
 });
 
-test('should generate end date action object', () => {
+test('should generate set end date action object', () => {
     const action = setEndDate(moment(0));
     expect(action).toEqual({
         type: "SET_END_DATE",
